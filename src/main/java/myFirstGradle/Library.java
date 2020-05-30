6c11c75343e9be56e61b16dceccce766491d5d90
Keith Jim <keithjim2002@hotmail.com>
2020-05-30 16:10:07 +0100

    Added member D

--- src/main/java/myFirstGradle/Library.java
+++ src/main/java/myFirstGradle/Library.java
@@ -7,6 +7,7 @@ public class Library {
     private int b;
     private int a;
     private int c;
+    private int d;
     private int e;
     private int g;
     public boolean someLibraryMethod() {
